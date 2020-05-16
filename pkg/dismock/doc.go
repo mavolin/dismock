e73a2183d7233c6b2c90cf35f9157be7722d85db
mavolin <48887425+mavolin@users.noreply.github.com>
2020-05-16 14:44:34 +0200

    docs(dismock): improve docs

--- pkg/dismock/doc.go
+++ pkg/dismock/doc.go
@@ -1,2 +1,4 @@
-// Package dismock creates mocks for the arikawa API wrapper.
+// Package dismock creates mocks for the Discord API.
+// The names of the mocks correspond to arikawa's API wrapper names, put as this are http mocks,
+// any discord library can be mocked.
 package dismock
