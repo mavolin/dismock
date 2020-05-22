15f33b761d53a50ccd13bc57b4faaed6fcebf176
mavolin <48887425+mavolin@users.noreply.github.com>
2020-05-22 19:38:36 +0200

    fix(sanitize): typo

--- internal/sanitize/invite.go
+++ internal/sanitize/invite.go
@@ -5,7 +5,7 @@ import "github.com/diamondburned/arikawa/discord"
 // Invite sanitizes an Invite.
 //
 // This function will sanitize Invite.Guild.ID, Invite.Guild.OwnerID,
-// Invite.Guild.Emojis.ID, Invite.Guild.Roles.ID, Invite.Channel.ID,
+// Invite.Guild.Emojis.ID, Invite.Guild.Roles.ID, Invite.Channel.ID and
 // Invite.Inviter.ID.
 func Invite(i discord.Invite, guildID, ownerID, channelID, inviterID, targetID discord.Snowflake) discord.Invite {
 	if i.Guild != nil {
